22fbf797aed1c2cb951cab74b76d4740c75c1a30
yonggyo1125 <yonggyo00@kakao.com>
2025-07-02 16:14:53 +0900

    .

---
+++ src/global/styles/color.js
@@ -0,0 +1,10 @@
+const color = {
+  primary: '#ffd139',
+  secondary: '#adb5bd',
+  success: '#198754',
+  info: '#0dcaf0',
+  warning: '#fd7e14',
+  danger: '#dc3545',
+  light: '#f8f9fa',
+  dark: '#212529',
+};
