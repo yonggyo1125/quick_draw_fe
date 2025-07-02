51a58df5bb629ccf6c692ac2ea5137ee8c0f9096
yonggyo1125 <yonggyo00@kakao.com>
2025-07-02 15:10:48 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
 
-function App() {
+const MainPage = loadable(() => import('./main/pages/MainPage'));
+const NotFoundPage = loadable(() => import('./global/pages/NotFoundPage'));
+const MainLayout = loadable(() => import('./global/layouts/MainLayout'));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<MainPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
