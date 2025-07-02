44525e8af002b4404964736f26d52bf512032874
yonggyo1125 <yonggyo00@kakao.com>
2025-07-02 15:21:50 +0900

    .

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ const MainPage = loadable(() => import('./main/pages/MainPage'));
 const NotFoundPage = loadable(() => import('./global/pages/NotFoundPage'));
 const MainLayout = loadable(() => import('./global/layouts/MainLayout'));
 
+/* drawing 페이지 */
+const DrawPage = loadable(() => import('./drawing/pages/DrawPage'));
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<MainPage />} />
+
+        <Route path="draw" element={<DrawPage />} />
 
         <Route path="*" element={<NotFoundPage />} />
       </Route>
---
+++ src/drawing/pages/DrawPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DrawPage = () => {
+  return <h1>Draw</h1>;
+};
+
+export default React.memo(DrawPage);
