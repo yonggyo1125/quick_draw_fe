e50d2088828334487c7d49d6e8eb976d9518410b
yonggyo1125 <yonggyo00@kakao.com>
2025-07-02 18:16:06 +0900

    .

--- src/main/pages/MainPage.js
+++ src/main/pages/MainPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import mainImage from '../../global/images/main.png';
+import { MediumButton } from '../../global/components/Buttons';
 
 const Wrapper = styled.div`
   position: absolute;
@@ -12,13 +13,28 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  .main-image {
+    display: block;
+    margin-bottom: 30px;
+  }
+
+  .start-btn {
+    width: 200px;
+    display: block;
+    margin: 0 auto;
+  }
 `;
 
 const MainPage = () => {
   return (
     <Wrapper className="layout-width">
-      <img src={mainImage} alt="메인이미지" />
-      <Link to="/draw"></Link>
+      <div>
+        <img className="main-image" src={mainImage} alt="메인이미지" />
+        <Link to="/draw" className="start-btn">
+          <MediumButton type="button">시작하기</MediumButton>
+        </Link>
+      </div>
     </Wrapper>
   );
 };
