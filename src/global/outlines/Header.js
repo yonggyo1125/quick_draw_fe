63dd02b7fbbd5837356bca1123f8434d180b261e
yonggyo1125 <yonggyo00@kakao.com>
2025-07-02 17:08:34 +0900

    .

--- src/global/outlines/Header.js
+++ src/global/outlines/Header.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+import { MdHome, MdDraw } from 'react-icons/md';
+import color from '../styles/color';
+import fontsize from '../styles/fontsize';
+
+const { primary, light, dark } = color;
+const { big, extraBig } = fontsize;
 
 const StyledHeader = styled.header`
+  height: 90px;
+  display: flex;
+  background: ${primary};
+  color: ${light};
+  align-items: center;
+  padding: 0 20px;
 
+  a {
+    font-size: ${big};
+    color: ${light};
+    display: flex;
+    align-items: center;
+
+    &.on {
+      text-shadow: 2px 2px 5px ${dark};
+    }
+
+    svg {
+      font-size: ${extraBig};
+    }
+  }
+
+  a + a {
+    margin-left: 25px;
+  }
 `;
 
 const Header = () => {
-  return <StyledHeader></StyledHeader>;
+  return (
+    <StyledHeader className="layout-width">
+      <NavLink to="/">
+        <MdHome />
+        <span>메인으로</span>
+      </NavLink>
+      <NavLink
+        to="/draw"
+        className={({ isActive }) => classNames({ on: isActive })}
+      >
+        <MdDraw />
+        <span>낙서하기</span>
+      </NavLink>
+    </StyledHeader>
+  );
 };
 
 export default React.memo(Header);
