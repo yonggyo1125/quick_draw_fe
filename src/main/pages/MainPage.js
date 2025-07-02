d8c7adb82c4aaf8583c77f13b4c1696b717447e2
yonggyo1125 <yonggyo00@kakao.com>
2025-07-02 17:47:08 +0900

    .

--- src/main/pages/MainPage.js
+++ src/main/pages/MainPage.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 import mainImage from '../../global/images/main.png';
+
+const Wrapper = styled.div`
+  position: absolute;
+  height: calc(100% - 190px);
+  width: 100%;
+  top: 90px;
+  left: 0;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
 
 const MainPage = () => {
   return (
-    <>
+    <Wrapper className="layout-width">
       <img src={mainImage} alt="메인이미지" />
-    </>
+    </Wrapper>
   );
 };
 
