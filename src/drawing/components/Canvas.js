1f15eefdd22db9bee2d5f32c437f45aaf1bf971a
yonggyo1125 <yonggyo00@kakao.com>
2025-07-03 17:23:03 +0900

    .

--- src/drawing/components/Canvas.js
+++ src/drawing/components/Canvas.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import color from '../../global/styles/color';
+
+const { dark } = color;
+
+const StyledCanvas = styled.canvas`
+  border: 1px solid ${dark};
+  margin-bottom: 15px;
+  border-radius: 3px;
+`;
 
 const Canvas = () => {
-  return <h1>그리는 곳</h1>;
+  return <StyledCanvas width="498" height="498"></StyledCanvas>;
 };
 
 export default React.memo(Canvas);
