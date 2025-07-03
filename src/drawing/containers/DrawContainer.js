708d324e1457fd0275734f5aa2b1b248126a031a
yonggyo1125 <yonggyo00@kakao.com>
2025-07-03 17:09:52 +0900

    .

--- src/drawing/containers/DrawContainer.js
+++ src/drawing/containers/DrawContainer.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import Canvas from '../components/Canvas';
 import Direction from '../components/Direction';
 import Result from '../components/Result';
+import { getRandomCategory } from '../global/categories';
 
 const DrawContainer = () => {
+  const [category, setCategory] = useState(() => getRandomCategory());
   return (
     <>
-      <Direction />
+      <Direction category={category} />
       <Canvas />
       <Result />
     </>
