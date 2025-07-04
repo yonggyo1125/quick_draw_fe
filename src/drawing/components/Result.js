0eb9cb0dfebfce70eb84a711f43a438bba8100cb
yonggyo1125 <yonggyo00@kakao.com>
2025-07-04 15:11:03 +0900

    .

--- src/drawing/components/Result.js
+++ src/drawing/components/Result.js
@@ -18,6 +18,23 @@ const Result = ({ onClick, category, eq, predictions }) => {
   );
 };
 
-const PredictionBox = ({ items, eq, category }) => {};
+const PredictionBox = ({ items, eq, category }) => {
+  return (
+    <div>
+      <div className="tit">
+        {eq
+          ? category + '(을)를 잘 그렸네요!'
+          : '잘 모르겠어요. 혹시 그린 것이 아래 있나요?'}
+      </div>
+      {!eq && (
+        <div>
+          {items.map((item) => (
+            <span key={item[0] + '_' + item[1]}></span>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default React.memo(Result);
