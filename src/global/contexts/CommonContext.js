732203cd1b0320a09a097ca265b255437aa99d87
yonggyo1125 <yonggyo00@kakao.com>
2025-07-08 16:58:37 +0900

    .

---
+++ src/global/contexts/CommonContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from 'react';
+
+const CommonContext = createContext({});
+
+const CommonProvider = ({ children }) => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [loggedMember, setLoggedMember] = useState({});
+
+  const value = {
+    state: { isLogin, loggedMember },
+    actions: { setIsLogin, setLoggedMember },
+  };
+
+  return (
+    <CommonContext.Provider value={value}>{children}</CommonContext.Provider>
+  );
+};
+
+export default CommonContext;
