4cda98128f2354a398e67dab46f6dd4992e0c455
yonggyo1125 <yonggyo00@kakao.com>
2025-07-08 18:15:30 +0900

    .

--- src/member/containers/LoginContainer.js
+++ src/member/containers/LoginContainer.js
@@ -9,13 +9,34 @@ const LoginContainer = () => {
     setForm((form) => ({ ...form, [e.target.name]: e.target.value }));
   }, []);
 
-  const onSubmit = useCallback((e) => {
-    e.preventDefault();
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
 
-    // 필수 항목 검증 S
+      const errors = {};
+      let hasErrors = false;
+      // 필수 항목 검증 S
+      const requiredFields = {
+        email: '이메일을 입력하세요.',
+        password: '비밀번호를 입력하세요.',
+      };
 
-    // 필수 항목 검증 E
-  }, []);
+      for (const [field, message] of Object.entries(requiredFields)) {
+        if (!form[field] || !form[field]?.trim()) {
+          errors[field] = message;
+          hasErrors = true;
+        }
+      }
+      // 필수 항목 검증 E
+      setErrors(errors);
+      if (hasErrors) {
+        return; // 검증 실패시 다음 로직 처리 X
+      }
+
+      // 로그인 처리..
+    },
+    [form],
+  );
 
   return <LoginForm form={form} onChange={onChange} onSubmit={onSubmit} />;
 };
