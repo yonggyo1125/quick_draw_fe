f8bf634fded9a96418e7cac802ed6adeaa62cbb6
yonggyo1125 <yonggyo00@kakao.com>
2025-07-08 17:43:56 +0900

    .

--- src/member/components/LoginForm.js
+++ src/member/components/LoginForm.js
@@ -4,7 +4,12 @@ import styled from 'styled-components';
 const StyledForm = styled.form``;
 
 const LoginForm = () => {
-  return <StyledForm></StyledForm>;
+  return (
+    <StyledForm>
+      <input type="text" name="email" placeholder="이메일" />
+      <input type="password" name="password" placeholder="비밀번호" />
+    </StyledForm>
+  );
 };
 
 export default React.memo(LoginForm);
