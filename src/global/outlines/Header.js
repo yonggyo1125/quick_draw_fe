ad7f66c6984a99c7b1d45021bcc6386ace0f6757
yonggyo1125 <yonggyo00@kakao.com>
2025-07-08 17:17:53 +0900

    .

--- src/global/outlines/Header.js
+++ src/global/outlines/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
-import { MdHome, MdDraw } from 'react-icons/md';
+import { MdHome, MdDraw, MdLogin } from 'react-icons/md';
 import color from '../styles/color';
 import fontsize from '../styles/fontsize';
 
@@ -40,17 +40,25 @@ const StyledHeader = styled.header`
 const Header = () => {
   return (
     <StyledHeader className="layout-width">
-      <NavLink to="/">
-        <MdHome />
-        <span>메인으로</span>
-      </NavLink>
-      <NavLink
-        to="/draw"
-        className={({ isActive }) => classNames({ on: isActive })}
-      >
-        <MdDraw />
-        <span>낙서하기</span>
-      </NavLink>
+      <div className="left">
+        <NavLink to="/">
+          <MdHome />
+          <span>메인으로</span>
+        </NavLink>
+        <NavLink
+          to="/draw"
+          className={({ isActive }) => classNames({ on: isActive })}
+        >
+          <MdDraw />
+          <span>낙서하기</span>
+        </NavLink>
+      </div>
+      <div className="right">
+        <NavLink to="/member/login">
+          <MdLogin />
+          <span>로그인</span>
+        </NavLink>
+      </div>
     </StyledHeader>
   );
 };
