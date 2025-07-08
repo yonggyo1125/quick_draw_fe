c421bc33329c4d3caddae310239fd9cfeb25572a
yonggyo1125 <yonggyo00@kakao.com>
2025-07-08 18:08:21 +0900

    .

--- src/member/containers/LoginContainer.js
+++ src/member/containers/LoginContainer.js
@@ -3,6 +3,7 @@ import LoginForm from '../components/LoginForm';
 
 const LoginContainer = () => {
   const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
 
   const onChange = useCallback((e) => {
     setForm((form) => ({ ...form, [e.target.name]: e.target.value }));
@@ -10,6 +11,10 @@ const LoginContainer = () => {
 
   const onSubmit = useCallback((e) => {
     e.preventDefault();
+
+    // 필수 항목 검증 S
+
+    // 필수 항목 검증 E
   }, []);
 
   return <LoginForm form={form} onChange={onChange} onSubmit={onSubmit} />;
