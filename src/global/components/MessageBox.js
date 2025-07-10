3ea6d46c527007f00ca09803c77943c809694b74
yonggyo1125 <yonggyo00@kakao.com>
2025-07-10 17:15:49 +0900

    .

--- src/global/components/MessageBox.js
+++ src/global/components/MessageBox.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import color from '../styles/color';
+import fontsize from '../styles/fontsize';
 
-const MessageBox = ({ items, children }) => {
+const { primary } = color;
+const { normal } = fontsize;
+
+const StyledItem = styled.div`
+  box-shadow: 2px 2px 5px ${primary};
+  color: ${primary};
+  padding: 5px 10px;
+  text-align: center;
+  border-radius: 3px;
+  background: #fff;
+
+  & + & {
+    margin-top: 5px;
+  }
+
+  ${({ theme }) =>
+    theme &&
+    color[theme] &&
+    css`
+      box-shadow: 2px 2px 5px ${color[theme]};
+      color: ${color[theme]};
+    `}
+`;
+
+const MessageBox = ({ items, theme, children }) => {
   if (children) items = children;
   items = Array.isArray(items) ? items : [items];
 
   return (
     <>
       {items.map((item, i) => (
-        <div key={item + '-' + i}>{item}</div>
+        <StyledItem key={item + '-' + i} theme={theme ?? 'primary'}>
+          {item}
+        </StyledItem>
       ))}
     </>
   );
